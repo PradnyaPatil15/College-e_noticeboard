ea2c415fac4d94d11948992b6a57aeb6ce0df155
Pradnya Patil <pradnyapatil1510@gmail.com>
2026-08-22 15:15:31 +0530

    Delete embeddings.py

--- embeddings.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-import numpy as np
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-
-def create_embedding(text):
-    embedding = model.encode(text)
-    return np.array(embedding).astype("float32")
